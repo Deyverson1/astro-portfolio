bd7833a7accde2ccd71434bd40ff6af01a957ebe
Deyverson1 <deyversongp@gmail.com>
2024-02-15 16:00:12 -0500

    Updates, working on Anime Section

---
+++ src/database/DataBaseAnime.ts
@@ -0,0 +1,17 @@
+export const dataBase = [
+  {
+    title: 'Naruto ナルト',
+    image: 'https://es.web.img2.acsta.net/pictures/13/12/13/08/50/378271.jpg',
+    synopsis: 'La obra narra la historia de un Ninja huérfano adolescente llamado Naruto Uzumaki, cuyos padres murieron en el ataque del Zorro Demonio de Nueve Colas (Kyubi), y quien aspira a convertirse en Hokage (líder de su aldea) con el propósito de ser reconocido como alguien importante dentro de la aldea y entre sus compañeros.',
+    fact: '"Este es mi camino Ninja"',
+    color: 'text-yellow-500',
+  },
+  {
+    title: 'Blue Lock ブルーロック Burū Rokku',
+    image: 'https://a.storyblok.com/f/178900/750x1060/781666be0a/bluelock-episode-nagi-main-visual.jpg/m/filters:quality(95)format(webp)',
+    synopsis: 'La selección de Japón terminó en el puesto 16 en la Copa Mundial de la FIFA 2018 . La Unión Japonesa de Fútbol contrata al enigma del fútbol Ego Jinpachi. Su plan maestro para llevar a Japón al estrellato es Blue Lock, un régimen de entrenamiento diseñado para crear el delantero egoísta más grande del mundo. A aquellos que fracasen en Blue Lock nunca más se les permitirá representar a Japón. Yoichi Isagi, un desconocido jugador de fútbol de secundaria que tiene conflictos sobre su estilo de juego, decide unirse al programa para convertirse en el mejor delantero del mundo.',
+    fact: '"Solo un verdadero egoísta puede tirar a puerta"',
+    color: 'text-blue-300'
+  }
+
+]
