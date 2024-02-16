e48720773e816de13fdce6323a82bda1d7cab524
Deyverson1 <deyversongp@gmail.com>
2024-02-16 09:14:10 -0500

    Anime done

--- src/database/DataBaseAnime.ts
+++ src/database/DataBaseAnime.ts
@@ -1,5 +1,19 @@
 export const dataBase = [
   {
+    title: 'Shingeki no Kyojin 進撃の巨人',
+    image: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/323c82257b2f6567fabbb7bd55bfa753.jpe',
+    synopsis: 'Hace siglos, la humanidad fue masacrada hasta casi su extinción por criaturas humanoides monstruosas llamadas Titanes, obligando a los humanos a esconderse por miedo detrás de enormes murallas concéntricas. Para asegurar su supervivencia, los restos de la humanidad comenzaron a vivir dentro de barreras defensivas, resultando en cien años sin un solo encuentro con titanes. Sin embargo, esa calma frágil pronto se ve destrozada cuando un Titán colosal logra atravesar la supuestamente inexpugnable muralla exterior. Después de presenciar una pérdida personal horrorosa a manos de las criaturas invasoras, Eren Yeager dedica su vida a su erradicación al alistarse en el Cuerpo de Exploración, una unidad militar de élite que combate a los despiadados humanoides fuera de la protección de las murallas. Eren, su hermana adoptiva Mikasa Ackerman y su amigo de la infancia Armin Arlert se unen a la brutal guerra contra los Titanes.',
+    fact: 'Shinzou wo sasageyo ❤️',
+    color: 'text-brown',
+  },
+  {
+    title: 'Tokyo Ghoul 東京喰種',
+    image: 'https://1.bp.blogspot.com/-N31CmbVNtNU/Xa1soqkaOlI/AAAAAAAAHX8/2YR_iRbJVgUAujeCJT4uUdiyjm8pfjSngCLcBGAsYHQ/s1600/tokyo%2Bghoul.jpeg',
+    synopsis: 'La historia sigue a Ken Kaneki, un estudiante universitario que apenas sobrevive a un encuentro mortal con Rize Kamishiro, su cita que se revela como una ghoul. Es llevado al hospital en condición crítica. Después de recuperarse, Kaneki descubre que fue sometido a una cirugía que lo transformó en medio ghoul. Esto se logró porque algunos de los órganos de Rize fueron trasplantados a su cuerpo, y ahora, como los ghouls normales, debe consumir carne humana para sobrevivir. Los ghouls que manejan la cafetería "Anteiku" lo acogen y le enseñan a lidiar con su nueva vida como medio ghoul. Algunas de sus luchas diarias incluyen encajar en la sociedad ghoul, así como mantener su identidad oculta de sus compañeros humanos, especialmente de su mejor amigo Hideyoshi Nagachika.',
+    fact: '¿Por qué debería disculparme por ser un monstruo? ¿Alguien se ha disculpado conmigo por convertirme en uno?',
+    color: 'text-gray-400'
+  },
+  {
     title: 'Naruto ナルト',
     image: 'https://es.web.img2.acsta.net/pictures/13/12/13/08/50/378271.jpg',
     synopsis: 'La obra narra la historia de un Ninja huérfano adolescente llamado Naruto Uzumaki, cuyos padres murieron en el ataque del Zorro Demonio de Nueve Colas (Kyubi), y quien aspira a convertirse en Hokage (líder de su aldea) con el propósito de ser reconocido como alguien importante dentro de la aldea y entre sus compañeros.',
@@ -12,6 +26,59 @@ export const dataBase = [
     synopsis: 'La selección de Japón terminó en el puesto 16 en la Copa Mundial de la FIFA 2018 . La Unión Japonesa de Fútbol contrata al enigma del fútbol Ego Jinpachi. Su plan maestro para llevar a Japón al estrellato es Blue Lock, un régimen de entrenamiento diseñado para crear el delantero egoísta más grande del mundo. A aquellos que fracasen en Blue Lock nunca más se les permitirá representar a Japón. Yoichi Isagi, un desconocido jugador de fútbol de secundaria que tiene conflictos sobre su estilo de juego, decide unirse al programa para convertirse en el mejor delantero del mundo.',
     fact: '"Solo un verdadero egoísta puede tirar a puerta"',
     color: 'text-blue-300'
-  }
+  },
+  {
+    title: 'Jujutsu Kaisen 呪術廻戦',
+    image: 'https://m.media-amazon.com/images/M/MV5BNGY4MTg3NzgtMmFkZi00NTg5LWExMmEtMWI3YzI1ODdmMWQ1XkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_FMjpg_UX1000_.jpg',
+    synopsis: 'Yuji Itadori, un adolescente de buen corazón, se une al Club de Ocultismo de su escuela por diversión, pero descubre que sus miembros son verdaderos hechiceros que pueden manipular la energía entre seres para su propio uso. Se entera de un talismán maldito, el dedo de Sukuna, un demonio, y de que otros seres malditos lo atacan. Yuji se come el dedo para proteger a sus amigos y termina convirtiéndose en el anfitrión de Sukuna. Sin embargo, Yuji descubre que ha heredado la magia y es capaz de controlar este poder sin la interferencia de Sukuna. Se une al Colegio Técnico de Magia Metropolitana de Tokio para consumir todos los dedos de Sukuna, lo que permitirá realizar un exorcismo completo que lo liberará.',
+    fact: 'You are my special',
+    color: 'text-red-500'
+  },
+  {
+    title: 'Death Note デスノート',
+    image: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/697c8b5ff2f49a97348b5507c06e1183.jpe',
+    synopsis: 'La historia sigue a Light Yagami, un genio estudiante de secundaria que descubre un misterioso cuaderno: el "Death Note", que pertenecía al shinigami Ryuk, y otorga al usuario la habilidad sobrenatural de matar a cualquier persona cuyo nombre sea escrito en sus páginas. La serie se centra en los intentos subsiguientes de Light de usar el Death Note para llevar a cabo una masacre mundial de individuos a quienes considera inmorales y crear una sociedad libre de crímenes, utilizando el alias de un vigilante tipo dios llamado "Kira", y los esfuerzos subsecuentes de un destacado equipo de tarea policial japonés, liderado por el enigmático detective L, para capturarlo.',
+    fact: 'El mundo humano es un lugar aburrido, con gente aburrida que hace cosas aburridas.',
+    color: 'text-red-600'
+  }, {
+    title: 'Nanatsu no Taizai 七つの大罪',
+    image: 'https://somoskudasai.com/wp-content/uploads/2020/08/7-taizai_teaser.jpg',
+    synopsis: 'Los Siete Pecados Capitales fueron una vez un grupo activo de caballeros en la región de Britannia, que se disolvieron después de que supuestamente conspiraron para derrocar el Reino de Liones. Su supuesta derrota llegó a manos de los Caballeros Sagrados, pero persistieron los rumores de que todavía estaban vivos. Diez años más tarde, los Caballeros Sagrados dieron un golpe de estado y capturaron al rey, convirtiéndose en los nuevos y tiránicos gobernantes del reino. La tercera princesa, Isabel, emprende un viaje para encontrar los Siete Pecados Capitales y conseguir su ayuda para recuperar el reino.',
+    fact: '¿Los conceptos como el bien y el mal existen realmente en la guerra?',
+    color: 'text-orange-600'
+  }, {
+    title: 'Demon Slayer: Kimetsu no Yaiba 鬼滅の刃',
+    image: 'https://es.web.img3.acsta.net/pictures/19/09/18/13/46/0198270.jpg',
+    synopsis: 'Kamado Tanjiro, un chico joven, bastante inteligente y con un corazón que no le cabe en el pecho. Este vive son su familia y gana dinero vendiendo carbón. Pero todo cambia cuando su familia es atacada y asesinada. Kamado descubre que ha sido culpa de Oni, un demonio bastante sobrenatural y sanguinario. Este, junto a su hermana Nezuko son los únicos supervivientes al ataque, pero su hermana fue convertida por el malvado demonio en una de su especie.  Tanjiro pasa de ser ese chico bueno que vendía carbón a un asesino de demonios para así poder ayudar a su hermana a volver a su forma humana nuevamente y vengar, por otro lado, la muerte de su familia y de sus seres queridos. ¿Conseguirá Kamado ayudar a su hermana y logrará cumplir el deseo de vencer a Oni, el demonio que acabó con su familia y así poder sentir que ha vengado la muerte de los que más quería ? ',
+    fact: 'Todas tus emociones... cargaré con ellas en el campo de batalla.',
+    color: 'text-blue-500'
+  }, {
+    title: 'Dragon Ball ドラゴンボール',
+    image: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/98ea4207e23a2ea3e3af39ad641d8800.jpe',
+    synopsis: 'La historia de Dragon Ball sigue la vida de Son Goku, un niño con cola de mono basado vagamente en el cuento folclórico chino tradicional Viaje al Oeste, desde su vida y aventuras como niño hasta convertirse en abuelo. Durante su vida, lucha en muchas batallas y eventualmente se convierte (discutiblemente) en el artista marcial más fuerte del universo. Sin embargo, no está solo: el cómic cuenta con un gran elenco de héroes y villanos artistas marciales que proporcionan el conflicto que impulsa la historia.',
+    fact: 'Soy el protector de los inocentes, soy la luz en la oscuridad',
+    color: 'text-yellow-500'
+  },
+  // {
+  //   title: '',
+  //   image: '',
+  //   synopsis: '',
+  //   fact: '',
+  //   color: ''
+  // },
+ {
+    title: 'Radiant ラディアン',
+    image: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/b5e1ce018b246cbc83bbf20a64fe8707.jpe',
+    synopsis: 'Seth es un joven que aspira a ser un gran hechicero que vive con Alma, una experta hechicera, en una remota isla. Los hechiceros son personas que han sobrevivido al contacto con los temibles némesis, y los humanos normales los temen como a los monstruos. Que Seth sea un joven hechicero hace que desconfíen de él, y que siempre esté causando escándalos no ayuda. Cuando un némesis ataca la isla, solo Seth podrá salvar a sus habitantes, los mismos que tanto parecen odiarlo. Por eso Seth tiene un objetivo claro: quiere acabar con todos los némesis del mundo.',
+    fact: 'Tu maldición no determina tu destino',
+    color: 'text-purple-500'
+  },
+   {
+    title: 'Yowamushi Pedal 弱虫ペダル',
+    image: 'https://m.media-amazon.com/images/S/pv-target-images/6c561d69a9bde1b4ec5cda7e272646ab4e0e1e79cd8f3a4462ddbe58a780a156.png',
+    synopsis: 'Sakamichi Onoda es un otaku que comienza a asistir a la Escuela Secundaria Sohoku. Le encanta el manga, los anime y los videojuegos, tanto es así que monta su bicicleta hacia el distrito comercial de Akihabara a una distancia de 90 kilómetros (aproximadamente 60 millas) de ida y vuelta sobre pendientes pronunciadas después de la escuela, solo para conseguir sus preciados objetos. La vida de Onoda cambia cuando se encuentra con Shunsuke Imaizumi y Shokichi Naruko, y se une al equipo de ciclismo de su escuela. Onoda termina por unirse al deporte de competición de carreras de bicicletas.',
+    fact: 'Por cortos que sean mis pasos, se irán sumando. Aunque hoy no haya ganado, los pasos darán su fruto algún día. Por eso no me rindo.',
+    color: 'text-green-500'
+  },
 
 ]
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brown: '#8B4513'
+			}
+		},
 	},
 	plugins: [],
 }
