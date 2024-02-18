8125e5338100836ccd34dffb47b63849507c0fd1
Deyverson1 <deyversongp@gmail.com>
2024-02-17 23:49:32 -0500

    Updates on music

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,7 @@
 import { useEffect, useRef, useState } from "react";
-
+import { data } from '../database/DataBaseMusic'
 function AudioPlayer() {
-  const data = [
-    { title: 'Someone To Spend Time With', subTitle: 'Los retros', src: 'music/Someone To Spend Time With.mp3', image: "https://i.scdn.co/image/ab67616d0000b273a1f060b534d1d9c859acc73f" },
-    { title: "Surround Sound (feat. 21 Savage & Baby Tate) ", subTitle: 'JID, 21 Savage, Baby Tate', src: 'music/JIDSurroundSound.mp3', image: 'https://i.scdn.co/image/ab67616d0000b2736de37e432e720323f4e31edc' },
-    { title: "Heathens", subTitle: 'Twenty One Pilots', src: 'music/Heathens.mp3', image: "https://i.scdn.co/image/ab67616d0000b2732ca3ba8f334ca5a5f0312efb" },
-    { title: "Another Love", subTitle: "Tom Odell", src: "music/AnotherLove.mp3", image: "https://i.scdn.co/image/ab67616d0000b2731917a0f3f4152622a040913f" },
-    { title: "Trouble", subTitle: "Coldplay", src: "music/Trouble.mp3", image: "https://i.scdn.co/image/ab67616d0000b2733d92b2ad5af9fbc8637425f0" },
-    { title: "Sex, Drugs, Etc.", subTitle: "Beach Weather", src: "music/Sex.mp3", image: "https://i.scdn.co/image/ab67616d0000b273d965622035c698fefc8c8a4a" },
-    { title: "Teeth", subTitle: "5 Seconds of Summer", src: "music/Teeth.mp3", image: "https://i.scdn.co/image/ab67616d0000b273726005f0f81903e157d9dbc7" },
-  ];
+
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -25,18 +17,18 @@ function AudioPlayer() {
 
   const handleCheck = () => {
     if (isHandleCheckRunning) {
-      return; // Evitar que la función se ejecute si ya está en ejecución
+      return;
     }
-  
+
     isHandleCheckRunning = true;
-  
+
     const timing = Math.floor(duration);
     if (timing == currentTime && checkBox.current.checked) {
       playNextTrack();
     } else {
       console.log('no activo');
     }
-  
+
     isHandleCheckRunning = false;
   };
 
@@ -62,8 +54,8 @@ function AudioPlayer() {
     const audioElement = audioRef.current;
 
     const handleEnded = () => {
-      if(checkBox.current.checked){
-      playNextTrack();
+      if (checkBox.current.checked) {
+        playNextTrack();
       }
     };
 
@@ -160,26 +152,26 @@ function AudioPlayer() {
   const progress = duration ? (currentTime / duration) * 100 : 0;
 
   const formattedTime = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-  const formattedDuration = `${totalDurationMinutes} : ${totalSecondDuration}`
+  const formattedDuration = `${totalDurationMinutes}:${totalSecondDuration}`
 
   return (
     <section className="pt-20 w-full ">
-      <label className="inline-flex w-full pr-4 items-center justify-end mb-5 cursor-pointer">
-        <input ref={checkBox} onClick={handleCheck} type="checkbox" value="" className="sr-only peer" />
-        <div className="relative w-11 h-6 bg-gray-20 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
-      </label>
-      <section className="pb-20 flex flex-col">
+      <section className="flex gap-4 px-44  mx-auto container pb-4 lg:max-w-4xl md:max-w-2xl">
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-playlist" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M17 17v-13h4" /><path d="M13 5h-10" /><path d="M3 9l10 0" /><path d="M9 13h-6" /></svg>
+        <h1 className="text-lg">Playlist</h1>
+      </section>
+
+      <section className="pb-28 flex flex-col  mx-auto container lg:max-w-4xl md:max-w-2xl">
         {data.map((dato, index) => (
           <article key={index} className="px-1 lg:px-40 cursor-pointer" onClick={(event) => handleClick(event, index)}>
-            <section className="hover:bg-gray-800 rounded-md">
+            <section className="hover:bg-gray-800 hover:text-white  rounded-md">
               <main className="flex gap-x-2 p-2 items-center ">
                 <div className="w-1/12 h-1/12">
                   <img src={dato.image} alt={`Image from ${dato.title}`} className="w-full rounded-md" />
                 </div>
                 <div>
-                  <h1 className="w-full hover:underline">{dato.title}</h1>
-                  <h2 className="text-sm text-gray-400">{dato.subTitle}</h2>
+                  <h1 className="w-full  hover:underline">{dato.title}</h1>
+                  <h2 className="text-sm  dark:text-gray-400">{dato.subTitle}</h2>
                 </div>
                 <div>
                 </div>
@@ -188,37 +180,58 @@ function AudioPlayer() {
           </article>
         ))}
       </section>
-      <section className="bottom-40 px-2 w-full pb-0 box-sizing">
+      <section className="fixed bottom-0 pb-4 justify-between px-2 py-4 w-full box-sizing flex bg-black  shadow-2xl shadow-white">
         <main className="w-full">
-          <div className="w-full">
-            <h1 className="font-lg ">{data[currentTrackIndex].title}</h1>
-            <h2 className="pb-4 text-sm text-gray-400">{data[currentTrackIndex].subTitle}</h2>
+          <div className="w-full flex gap-2 pl-2 items-center">
+            <img src={data[currentTrackIndex].image} alt="" className="h-14 rounded-md" />
+            <div className="flex flex-col pl-2 justify-center">
+              <h1 className="font-xs text-white leading-5 tracking-tight">{data[currentTrackIndex].title}</h1>
+              <h2 className="text-sm text-gray-400">{data[currentTrackIndex].subTitle}</h2>
+            </div>
             <audio ref={audioRef}>
               <source src={data[currentTrackIndex].src} type="audio/mpeg" />
               Tu navegador no soporta el elemento de audio.
             </audio>
           </div>
         </main>
+        <section className="w-full flex flex-col justify-center items-center">
+          <section className="w-full gap-8 pb-1 flex items-center justify-center">
+            <label className="hidden cursor-pointer items-center justify-center">
+              <input ref={checkBox} checked onClick={handleCheck} type="checkbox" value="" className="sr-only peer" />
+              <div className="relative w-11 h-6 bg-gray-20 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+            </label>
+            <button onClick={playPreviousTrack}><svg xmlns="http://www.w3.org/2000/svg" className=" size-7 icon icon-tabler icon-tabler-chevron-left" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg></button>
+            <button onClick={togglePlayPause}>{isPlaying ? <svg xmlns=" http://www.w3.org/2000/svg" className="dark:fill-white size-7 icon icon-tabler icon-tabler-player-pause" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="none" fill="#fff" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /><path d="M14 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" className=" dark:fill-white size-7 icon icon-tabler icon-tabler-player-play-filled" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="#fff" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" strokeWidth="0" fill="white" /></svg>}</button>
+            <button onClick={playNextTrack}><svg xmlns="http://www.w3.org/2000/svg" className="size-7 icon icon-tabler icon-tabler-chevron-right" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 6l6 6l-6 6" /></svg></button>
+          </section>
 
-        <div
-          className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700"
-          ref={progressBarRef}
-          onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-          onClick={handleProgressBarClick} 
-        >
-          <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${progress}%` }}></div>
-        </div>
+          <section className="flex w-full justify-center items-center gap-2">
 
-        <div className="w-full flex justify-between pt-2">
-          <p>{formattedTime}</p>
-          <p>{formattedDuration}</p>
-        </div>
-        <section className="w-full pt-4 gap-8 flex items-center justify-center">
-          <button onClick={playPreviousTrack}><svg xmlns="http://www.w3.org/2000/svg" className="size-12 icon icon-tabler icon-tabler-chevron-left" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg></button>
-          <button onClick={togglePlayPause}>{isPlaying ? <svg xmlns=" http://www.w3.org/2000/svg" className="size-12 icon icon-tabler icon-tabler-player-pause" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /><path d="M14 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" className="size-12 icon icon-tabler icon-tabler-player-play-filled" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" strokeWidth="0" fill="currentColor" /></svg>}</button>
-          <button onClick={playNextTrack}><svg xmlns="http://www.w3.org/2000/svg" className="size-12 icon icon-tabler icon-tabler-chevron-right" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 6l6 6l-6 6" /></svg></button>
+            <p className="text-sm">{formattedTime}</p>
+            <div
+              className="w-full gap-8 bg-gray-200 flex justify-between items-center rounded-full h-1 dark:bg-gray-700"
+              ref={progressBarRef}
+              onMouseDown={handleMouseDown}
+              onMouseMove={handleMouseMove}
+              onMouseUp={handleMouseUp}
+              onClick={handleProgressBarClick}
+            >
+              <div className="bg-blue-600 h-1 rounded-full" style={{ width: `${progress}%` }}></div>
+            </div>
+            <p className="text-sm">{formattedDuration}</p>
+          </section>
+        </section>
+        <section className=" px-2 w-full pb-0 flex">
+          <main className="w-full">
+            <div className="w-full flex gap-2 items-center justify-end">
+              <img src={data[currentTrackIndex].image} alt="" className="h-10" />
+              <div className="flex flex-col justify-center">
+                <h1 className="font-lg ">{data[currentTrackIndex].title}</h1>
+                <h2 className="text-sm text-gray-400">{data[currentTrackIndex].subTitle}</h2>
+              </div>
+            </div>
+          </main>
         </section>
       </section>
     </section>
---
+++ src/database/DataBaseMusic.ts
@@ -0,0 +1,19 @@
+export const data = [
+  { title: 'Someone To Spend Time With', subTitle: 'Los Retros', src: 'music/Someone To Spend Time With.mp3', image: "https://i.scdn.co/image/ab67616d0000b273a1f060b534d1d9c859acc73f" },
+  { title: "Everything Black", subTitle: 'Unlike Pluto, Mike Taylor', src: 'music/EverythingBlack.mp3', image: 'https://i.scdn.co/image/ab67616d0000b27396957b0279e407485635f831' },
+  { title: "Surround Sound (feat. 21 Savage & Baby Tate) ", subTitle: 'JID, 21 Savage, Baby Tate', src: 'music/JIDSurroundSound.mp3', image: 'https://i.scdn.co/image/ab67616d0000b2736de37e432e720323f4e31edc' },
+  { title: "Heathens", subTitle: 'Twenty One Pilots', src: 'music/Heathens.mp3', image: "https://i.scdn.co/image/ab67616d0000b2732ca3ba8f334ca5a5f0312efb" },
+  { title: "Another Love", subTitle: "Tom Odell", src: "music/AnotherLove.mp3", image: "https://i.scdn.co/image/ab67616d0000b2731917a0f3f4152622a040913f" },
+  { title: "Sex, Drugs, Etc.", subTitle: "Beach Weather", src: "music/Sex.mp3", image: "https://i.scdn.co/image/ab67616d0000b273d965622035c698fefc8c8a4a" },
+  { title: "Teeth", subTitle: "5 Seconds of Summer", src: "music/Teeth.mp3", image: "https://i.scdn.co/image/ab67616d0000b273726005f0f81903e157d9dbc7" },
+  { title: "goosebumps", subTitle: 'Travis Scott', src: 'music/goosebumps.mp3', image: 'https://i.scdn.co/image/ab67616d0000b273f54b99bf27cda88f4a7403ce' },
+  { title: "XO Tour Llif3", subTitle: 'Lil Uzi Vert', src: 'music/XoTourLife.mp3', image: 'https://i.scdn.co/image/ab67616d0000b27341b41e6f7924e530a9b00f9c' },
+  { title: "What They Want", subTitle: 'Russ', src: 'music/WhatTheyWant.mp3', image: 'https://i.scdn.co/image/ab67616d0000b273cb045e684adce8d49ada4045' },
+  { title: "Trouble", subTitle: "Coldplay", src: "music/Trouble.mp3", image: "https://i.scdn.co/image/ab67616d0000b2733d92b2ad5af9fbc8637425f0" },
+  { title: "idfc", subTitle: 'blackbear', src: 'music/idfc.mp3', image: 'https://i.scdn.co/image/ab67616d00001e026ac1ad714f815e19c2372401' },
+  { title: "High Enough", subTitle: 'K.Flay', src: 'music/High.mp3', image: 'https://i.scdn.co/image/ab67616d0000b273be9f834ad14e0d95a35a6981' },
+  { title: "Falling", subTitle: 'Trevor Daniel', src: 'music/falling.mp3', image: 'https://i.scdn.co/image/ab67616d0000b273f3508855e1fd44d447a3413b' },
+  { title: "rockstar", subTitle: 'Post Malone, 21 Savage', src: 'music/rockstar.mp3', image: 'https://i.scdn.co/image/ab67616d0000b273b1c4b76e23414c9f20242268' },
+  { title: "Overwhelmed (Ryan Mack Remix)", subTitle: 'Ryan Mack', src: 'music/overwhelmed.mp3', image: 'https://i.scdn.co/image/ab67616d0000b273406c79c19c702998ece57d7f' },
+
+];
