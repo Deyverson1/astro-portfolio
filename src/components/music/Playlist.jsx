0c5d7a2f0c9492bdbee2af3ebe57124f852db28a
Deyverson1 <deyversongp@gmail.com>
2024-03-20 14:24:49 -0500

    Updates on colors

--- src/components/music/Playlist.jsx
+++ src/components/music/Playlist.jsx
@@ -4,7 +4,7 @@ export default function PlayList({ data, handleClick }) {
     <section className="pb-6 flex flex-col  mx-auto container lg:max-w-4xl md:max-w-2xl">
       {data.map((dato, index) => (
         <article key={index} className="px-1 lg:px-40 cursor-pointer" onClick={(event) => handleClick(event, index)}>
-          <section className="hover:bg-gray-800 hover:text-white  rounded-md">
+          <section className="hover:bg-gray-500 dark:hover:bg-gray-800 hover:text-white  rounded-md">
             <main className="flex gap-x-2 p-2 items-center ">
               <div className="w-1/12 h-1/12">
                 <img src={dato.image} alt={`Image from ${dato.title}`} className="w-full rounded-md" />
