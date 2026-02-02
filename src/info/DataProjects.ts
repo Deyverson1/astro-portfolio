be314f2c6c65f73f392a34af10c6755d24f7fcba
Deyverson1 <deyversongp@gmail.com>
2026-02-01 19:27:26 -0500

    Cambios de imagenes

--- src/info/DataProjects.ts
+++ src/info/DataProjects.ts
@@ -33,7 +33,7 @@ export const projects = [
     link: '',
     github:
       'https://github.com/Deyverson1/clone-respositories/blob/master/clone-repos.py',
-    image: '/projects/script-repos.png',
+    image: 'CloneRepos.png',
     tags: [tags.Python],
     color: 'border-blue-600',
     id: '',
@@ -44,34 +44,34 @@ export const projects = [
       'Este proyecto tiene objetivo la replica/adaptación tanto de la Landing page de la pagina web https://www.netflix.com/co/, como de su browser principal el cual se encarga de la búsqueda de contenido disponible en la plataforma de Netflix ',
     link: 'https://netflix-replic.vercel.app/',
     github: 'https://github.com/Deyverson1/netflix-replic',
-    image: '/projects/netflix-replic.png',
+    image: 'NetflixImage.png',
     tags: [tags.TypeScript, tags.React, tags.Tailwind],
     color: 'border-red-500',
     id: '5',
   },
-  {
-    title: 'Anime Search - Consumo de APIs',
-    description:
-      'Tiene como objetivo la implementación de búsqueda de anime a partir del consumo de varias APIS como https://jikan.moe/, https://animechan.xyz/, https://nekos.best/, presento una implementación de TypeScript para mejorar la calidad del código con respecto a los errores',
-    github: 'https://github.com/Deyverson1/anime-search',
-    link: 'https://anime-search-deyverson1.vercel.app/',
-    image: '/projects/animesearch.webp',
-    tags: [tags.TypeScript, tags.Tailwind, tags.React],
-    color: 'border-yellow-600',
-    id: '2',
-  },
+  // {
+  //   title: 'Anime Search - Consumo de APIs',
+  //   description:
+  //     'Tiene como objetivo la implementación de búsqueda de anime a partir del consumo de varias APIS como https://jikan.moe/, https://animechan.xyz/, https://nekos.best/, presento una implementación de TypeScript para mejorar la calidad del código con respecto a los errores',
+  //   github: 'https://github.com/Deyverson1/anime-search',
+  //   link: 'https://anime-search-deyverson1.vercel.app/',
+  //   image: '/projects/animesearch.webp',
+  //   tags: [tags.TypeScript, tags.Tailwind, tags.React],
+  //   color: 'border-yellow-600',
+  //   id: '2',
+  // },
 
-  {
-    title: 'Search Movies and books!',
-    description:
-      'Fue creado con el propósito de aprender acerca de Svelte, actualmente presenta dos funciones principales las cuales son la búsqueda de libros y películas',
-    link: 'https://search-movies-and-books.vercel.app/',
-    github: 'https://github.com/Deyverson1/search-movies-and-books',
-    image: '/projects/searchmovie.webp',
-    tags: [tags.Svelte, tags.JavaScript, tags.Css],
-    color: 'border-sky-500',
-    id: '4',
-  },
+  // {
+  //   title: 'Search Movies and books!',
+  //   description:
+  //     'Fue creado con el propósito de aprender acerca de Svelte, actualmente presenta dos funciones principales las cuales son la búsqueda de libros y películas',
+  //   link: 'https://search-movies-and-books.vercel.app/',
+  //   github: 'https://github.com/Deyverson1/search-movies-and-books',
+  //   image: '/projects/searchmovie.webp',
+  //   tags: [tags.Svelte, tags.JavaScript, tags.Css],
+  //   color: 'border-sky-500',
+  //   id: '4',
+  // },
   {
     title: 'Proyecto de CRUD en PHP',
     description:
@@ -92,7 +92,7 @@ export const projects = [
       'https://github.com/Deyverson1/SHT-30-Humidity-HC-SR04-Distance-Termistor-Temperatura',
     link: '',
     documentation: '/mechatronics/documentation',
-    image: '/projects/sensores.webp',
+    image: 'Sensores.png',
     color: 'border-pink-600',
     tags: [tags.C],
   },
